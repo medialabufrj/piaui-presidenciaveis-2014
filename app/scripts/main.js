0a1784d9754d0407ac7f86aaa9cc1cb0d4dd6ec6
Marlus Araujo <m@marlus.com>
2014-09-11 13:13:26 -0300

    primeiros arquivos CSV, underscore e carregamento de varios arquivos CSV por DSV

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,67 +1,98 @@
 
 // DATASETS
 
-var data_candidatos = [],
-    data_eventos    = [],
-    data_categorias = [],
-    data_regioes    = ["Norte","Nordeste","Centro-oeste","Sudeste","Sul"],
+var data_regioes    = ["Norte","Nordeste","Centro-oeste","Sudeste","Sul"],
     data_estados    = [
 
-    {uf: "ac", regiao: 0},
-    {uf: "am", regiao: 0},
-    {uf: "ap", regiao: 0},
-    {uf: "pa", regiao: 0},
-    {uf: "rr", regiao: 0},
-    {uf: "ro", regiao: 0},
-    {uf: "to", regiao: 0},
+        {uf: "AC", regiao: 0},
+        {uf: "AM", regiao: 0},
+        {uf: "AP", regiao: 0},
+        {uf: "PA", regiao: 0},
+        {uf: "RR", regiao: 0},
+        {uf: "RO", regiao: 0},
+        {uf: "TO", regiao: 0},
 
-    {uf: "ma", regiao: 1},
-    {uf: "pi", regiao: 1},
-    {uf: "ce", regiao: 1},
-    {uf: "ba", regiao: 1},
-    {uf: "rn", regiao: 1},
-    {uf: "pb", regiao: 1},
-    {uf: "pe", regiao: 1},
-    {uf: "al", regiao: 1},
-    {uf: "se", regiao: 1},
+        {uf: "MA", regiao: 1},
+        {uf: "PI", regiao: 1},
+        {uf: "CE", regiao: 1},
+        {uf: "BA", regiao: 1},
+        {uf: "RN", regiao: 1},
+        {uf: "PB", regiao: 1},
+        {uf: "PE", regiao: 1},
+        {uf: "AL", regiao: 1},
+        {uf: "SE", regiao: 1},
 
-    {uf: "mt", regiao: 2},
-    {uf: "go", regiao: 2},
-    {uf: "ms", regiao: 2},
-    {uf: "df", regiao: 2},
+        {uf: "MT", regiao: 2},
+        {uf: "GO", regiao: 2},
+        {uf: "MS", regiao: 2},
+        {uf: "DF", regiao: 2},
 
-    {uf: "es", regiao: 3},
-    {uf: "mg", regiao: 3},
-    {uf: "rj", regiao: 3},
-    {uf: "sp", regiao: 3},
+        {uf: "ES", regiao: 3},
+        {uf: "MG", regiao: 3},
+        {uf: "RJ", regiao: 3},
+        {uf: "SP", regiao: 3},
 
-    {uf: "pr", regiao: 4},
-    {uf: "sc", regiao: 4},
-    {uf: "rs", regiao: 4}
+        {uf: "PR", regiao: 4},
+        {uf: "SC", regiao: 4},
+        {uf: "RS", regiao: 4}
 
-];
+    ],
+    data_candidatos = [],
+    data_eventos    = [],
+    data_categorias = []
+    ;
 
 
 // PROCESSAMENTO DE DADOS
 
-d3.csv("data.csv")
-    .row(function(d){
-        // alimenta array candidatos
-        if(data_candidatos.indexOf(d.candidato) == -1){
-            data_candidatos.push(d.candidato);
-        }
-        // alimenta array categorias
-        if(data_categorias.indexOf(d.categoria) == -1){
-            data_categorias.push(d.categoria);
-        }
-        // retorna obj completo
-        return d;
-    })
-    .get(function(error, rows){
-        // alimenta array eventos
-        data_eventos = rows;
-    });
+function loadDataset(arr) {
+    var count = 0;
+    var callback = function(){
+        count++;
+        if(count < arr.length)
+            loadCSV(arr[count], callback);
+    }
+    loadCSV(arr[0],callback);
+}
 
+function loadCSV(file,callback){
+
+    // DVS para carregar CSV separado por ;
+
+    var dsv = d3.dsv(";", "text/plain");
+
+    dsv(file)
+        .row(function(d){
+            // alimenta array candidatos
+            if(data_candidatos.indexOf(d.CANDIDATO) == -1){
+                data_candidatos.push(d.CANDIDATO);
+            }
+            // alimenta array categorias
+            if(data_categorias.indexOf(d.CATEGORIA) == -1){
+                data_categorias.push(d.CATEGORIA);
+            }
+            // adiciona regiao
+            d.REGIAO = data_regioes[_.findWhere(data_estados,{uf: d.UF}).regiao];
+            // retorna obj completo
+            return d;
+        })
+        .get(function(error, rows){
+            // alimenta array eventos
+            data_eventos = _.union(rows,data_eventos);
+            // log
+            console.log('loaded CSV!');
+            // chama callback
+            if(callback) callback();
+        });
+
+    console.log('loading file', file);
+}
+
+loadDataset([
+    'dados_presidenciaveis_revistapiaui_aecioneves.csv',
+    'dados_presidenciaveis_revistapiaui_dilmarousseff.csv',
+    'dados_presidenciaveis_revistapiaui_marinasilva.csv'
+]);
 
 
 // INICIA GRAFOS
